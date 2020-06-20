66f15850bb246ccf2687757d20b0c05eb688d1bc
D.Hrulev <dmitryhrulev@gmail.com>
2020-06-20 12:17:43 +0300

    List of todo with add/remove comp, w/o routing

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h1>Todo aplicable</h1>
+    <AddTodo 
+      @add-todo='addTodo'
+    />
+    <hr />
+    <TodoList 
+    v-bind:todos="todos" 
+    @remove-todo='removeTodo'
+    />
+  </div>
+</template>
+
+<script>
+
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        {id: 1, title: 'Купить хлеб', completed: false},
+        {id: 2, title: 'Купить молоко', completed: false},
+        {id: 3, title: 'Купить картоху', completed: false},
+      ]
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(item => item.id !== id )
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList, AddTodo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li>
+    <span :class="{done: todo.completed}">
+      <input type="checkbox" @click="todo.completed = !todo.completed" />
+      <strong> {{index+1}} </strong>
+      {{todo.title}}
+    </span>
+    <button class="rm" @click="$emit('remove-todo', todo.id)">&times;</button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    index: Number
+  }
+};
+</script>
+
+<style scoped>
+li{
+   border: 1px solid #ccc;
+   display: flex;
+   justify-content: space-between;
+   padding: .5rem 2rem;
+   margin-bottom: 1rem;
+}
+.rm {
+   background: orangered;
+   color: #ffffff;
+   border-radius: 50%;
+   font-weight: bold;
+}
+.done {
+   text-decoration: line-through;
+}
+input{
+   margin-right: 1rem;
+}
+</style>
